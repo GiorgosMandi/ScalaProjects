04ed12515253cd7d0ca9d54c10d18349937dddfc
George Mandilaras <gmandilaras@ubitech.eu>
2023-06-28 17:59:17 +0300

    spark-streaming
    init

---
+++ spark-projects/src/main/scala/sparkStreaming/common/Stock.scala
@@ -0,0 +1,9 @@
+package sparkStreaming.common
+
+import java.sql.Date
+
+case class Stock(
+                  company: String,
+                  date: Date,
+                  value: Double
+                )
